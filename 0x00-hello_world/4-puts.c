63fb71f564876735d6dc290b766d2551b8f1ae1b
Crownezt <adepheranmie@gmail.com>
2023-10-05 12:34:50 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints exactly
+ * "Programming is like building a multilingual puzzle
+ * using function puts
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+first directory - C programming language
