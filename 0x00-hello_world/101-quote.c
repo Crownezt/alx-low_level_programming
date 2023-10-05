d3ef4617ee5d29bc43e1d1a766ddb92e298176da
Crownezt <adepheranmie@gmail.com>
2023-10-05 14:04:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
-/**main - a C program that prints exactly and that
- * piece of art is useful" - Dora Korpar, 2015-10-19,
+
+/**
+ * main - a C program that prints exactly and that
+ * piece of art is useful\" - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error
  * You are not allowed to use any functions listed
  * in the NAME section of the man (3) printf or man (3) puts
  * Your program should return 1
+ *
+ * Return: 0 upon success
  */
 
 int main(void)
