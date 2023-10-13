2610618b9a3a4b279f2e04cf5c4b38a449ffd4c9
Crownezt <adepheranmie@gmail.com>
2023-10-13 12:41:01 +0100

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper(int c): a function that checks for uppercase character.
+ * _isupper - a function that checks for uppercase character.
  * @c: the character to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
